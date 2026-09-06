99bfe0783f0972ff1f96429c38fd25d5e100f2f2
Stradalec <pochtatipo6@gmail.com>
2026-09-06 20:35:22 +0700

    fix: new template

--- EditorApp/source/ViewModel.cs
+++ EditorApp/source/ViewModel.cs
@@ -39,7 +39,7 @@ namespace EditorApp.source
         public ObservableCollection<TemplateItem> Templates { get; } = new ObservableCollection<TemplateItem>
         {
             new TemplateItem { Id = "default", Title = "ГОСТ Р 7.0.100-2018" },
-            new TemplateItem { Id = "test", Title = "Тест" }
+            new TemplateItem { Id = "strict", Title = "ГОСТ Р 7.0.100-2018 (строгий)" }
         };
 
         [ObservableProperty]

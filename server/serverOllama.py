4d8f4122fe2cc7089eee4791489c071bd72d19be
Andrey Kalinin <pochtatipo6@gmail.com>
2026-09-06 19:40:57 +0700

    feature: now work with true prod

--- server/serverOllama.py
+++ server/serverOllama.py
@@ -372,8 +372,8 @@ if __name__ == "__main__":
         
         serve(
             app,
-            host="0.0.0.0",
-            port=44752,
+            host="127.0.0.1",
+            port=44753,
             threads=4
         )
         
